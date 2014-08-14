19f887f9d75ae0df5549bb3b0beb98d408068de4
shouldbee <hello@craftsman-software.com>
2014-08-14 08:35:20 +0000

    version: 36e5449

--- Formula/shouldbee.rb
+++ Formula/shouldbee.rb
@@ -2,14 +2,14 @@ require "formula"
 
 class Shouldbee < Formula
   homepage "http://shouldbee.at"
-  version "e5f98f4"
+  version "36e5449"
 
   if MacOS.prefer_64_bit?
-    url "https://github.com/shouldbee/homebrew-shouldbee/raw/e5f98f4/build/darwin-amd64/shouldbee"
-    sha1 "33c91a75b2011c819d8a045d71f2fc2d46fc5c1f"
+    url "https://github.com/shouldbee/homebrew-shouldbee/raw/36e5449/build/darwin-amd64/shouldbee"
+    sha1 "51462d0a0de884079f04b41e0e0af4a6af3bd9b9"
   else
-    url "https://github.com/shouldbee/homebrew-shouldbee/raw/e5f98f4/build/darwin-386/shouldbee"
-    sha1 "fbe5cf994ad377822c0be3faa5370c245a3b2303"
+    url "https://github.com/shouldbee/homebrew-shouldbee/raw/36e5449/build/darwin-386/shouldbee"
+    sha1 "dedd8d7e27d6129f8022eeebf9d768bd92a861e9"
   end
 
   def install
